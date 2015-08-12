9314ddbc75d42af51a1f040e3413d9e69e5301cc
eduardoacye <eduardo.acye@gmail.com>
2015-08-11 23:17:30 -0700

    Agregado Crawler Code

--- collective-intelligence/searchengine.py
+++ collective-intelligence/searchengine.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
+
+ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+
 class crawler:
     # Inicializa el crawler con el nombre de la base de datos
     def __init__(self, dbname):
@@ -39,7 +45,32 @@ class crawler:
     # Comenzando con una lista de páginas, hacer una búsqueda a lo ancho
     # a una profundidad dada, indexando las páginas en el proceso
     def crawl(self, pages, depth=2):
-        pass
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print "Could not open %s" % page
+                    continue
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page, soup)
+
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page, link['href'])
+                        if url.find("'") != -1:
+                            continue
+                        url = url.split('#')[0] # Remueve la parte de localización
+                        if url[0:4] == 'http' and not self.isindexed(url):
+                            newpages.add(url)
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page, url, linkText)
+
+                self.dbcommit()
+
+            pages = newpages
 
     # Crear las tablas de base de datos
     def createindextables(self):
