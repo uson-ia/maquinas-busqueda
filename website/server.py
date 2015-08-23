0319727d2265e0a93fe9d43da56f3ab8ee44061a
eduardoacye <eduardo.acye@gmail.com>
2015-08-22 22:04:20 -0700

    agregado correcto manejo de datos entre handlers

--- website/server.py
+++ website/server.py
@@ -1,32 +1,33 @@
+# -*- coding: utf-8 -*-
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url, StaticFileHandler
 import os.path
 
-foofle_query = ""
-foofle_results = [[10, "http://www.uson.mx"],
-                  [9, "https://www.wikipedia.org"],
-                  [8, "https://www.google.com.mx"]]
-#foofle_results = []
+foofle_data = {"query" : "",
+               "results" : []}
+
+def update_data(query):
+    foofle_data["query"] = query
+    foofle_data["results"] = [[10, "http://" + query]]
 
 class MainHandler(RequestHandler):
-    def initialize(self, database):
-        self.database = database
-        self.foofle2_query = "Hola"
-        self.foofle2_results = []
+    def initialize(self, data):
+        self.data = data
 
     def get(self):
-        #self.render("index.html", query = foofle_query, results = foofle_results)
-        self.render("index.html", query = self.foofle2_query, results = self.foofle2_results)
+        self.render("index.html", query = self.data["query"], results = self.data["results"])
 
     def post(self):
-        print "In POST method"
-        print self.get_argument("input-query")
+        query = self.get_argument("input-query")
+        print "La busqueda que se realizara utilizara la cadena '%s' como consulta" % query
+        update_data(query)
+        self.get()
 
 
 def make_app():
     return Application(
         [
-            url(r"/", MainHandler, dict(database={})),
+            url(r"/", MainHandler, {"data" : foofle_data}),
             url(r"/(.*)", StaticFileHandler, {"path":"./ui"})
         ],
         template_path = os.path.join(os.path.dirname(__file__), "ui"))
