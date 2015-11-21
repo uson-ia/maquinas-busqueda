494311ade7904f52624ec8cc9b7fb44590c0d4fe
cruzluquejm <cruzluquejm@gmail.com>
2015-11-21 14:30:43 -0700

    ..

--- nn.py
+++ nn.py
@@ -3,6 +3,9 @@ __author__ = 'JuanManuel'
 from math import tanh
 from pysqlite2 import dbapi2 as sqlite
 
+def dtanh(y):
+    return 1.0 - y * y
+
 class searchnet:
     def __init__(self, dbname):
         self.con = sqlite.connect(dbname)
@@ -18,9 +21,9 @@ class searchnet:
 
     def getstrength(self, fromid, toid, layer):
         if layer == 0: 
-            table = 'wordhidden'
+        	table = 'wordhidden'
         else: 
-            table = 'hiddenurl'
+        	table = 'hiddenurl'
 
         res = self.con.execute('select strength from %s where \
                 fromid = %d and toid = %d' % (table, fromid,
@@ -28,17 +31,17 @@ class searchnet:
 
         if res == None:
             if layer == 0: 
-                return 0.2
+            	return 0.2
             if layer == 1: 
-                return 0
+            	return 0
 
         return res[0]
 
     def setstrength(self, fromid, toid, layer, strength):
         if layer == 0: 
-            table = 'wordhidden'
+        	table = 'wordhidden'
         else: 
-            table = 'hiddenurl'
+        	table = 'hiddenurl'
 
         res = self.con.execute('select rowid from %s where \
                 fromid = %d and toid = %d' % (table, fromid,
@@ -54,7 +57,7 @@ class searchnet:
 
     def generatehiddennode(self, wordids, urls):
         if len(wordids) > 3: 
-            return None
+        	return None
         # Comprobar si ya creamos un nodo para este conjunto de palabras
         createkey = '_'.join(sorted([str(wi) for wi in wordids]))
         res = self.con.execute("select rowid from hiddennode where create_key \
@@ -73,6 +76,116 @@ class searchnet:
                 self.setstrength(hiddenid, urlid, 1, 0.1)
             self.con.commit()
 
+    def getallhiddenids(self, wordids, urlids):
+        l1 = {}
+        for wordid in wordids:
+            cur = self.con.execute('select toid from wordhidden where \
+                    fromid = %d' % wordid)
+            for row in cur: 
+            	l1[row[0]] = 1
+        for urlid in urlids:
+            cur = self.con.execute('select fromid from hiddenurl where \
+                    toid = %d' % urlid)
+            for row in cur: l1[row[0]] = 1
+
+        return l1.keys()
+
+    def setupnework(self, wordids, urlids):
+        # Listas de valores
+        self.wordids = wordids
+        self.hiddenids = self.getallhiddenids(wordids, urlids)
+        self.urlids = urlids
+
+        # Salidas del nodo
+        self.ai = [1.0] * len(self.wordids)
+        self.ah = [1.0] * len(self.hiddenids)
+        self.ao = [1.0] * len(self.urlids)
+
+        # Crear matriz de pesos
+        self.wi = [[self.getstrength(wordid, hiddenid, 0)
+            for hiddenid in self.hiddenids]
+            for wordid in self.wordids]
+        self.wo = [[self.getstrength(hiddenid, urlid, 1)
+            for urlid in self.urlids]
+            for hiddenid in self.hiddenids]
+
+    def feedforward(self):
+        # Las unicas entradas son las palabras de la consulta
+        for i in range(len(self.wordids)):
+            self.ai[i] = 1.0
+
+        # Activaciones ocultas
+        for j in range(len(self.hiddenids)):
+            sum = 0.0
+            for i in range(len(self.wordids)):
+                sum = sum + self.ai[i] * self.wi[i][j]
+            self.ah[j] = tanh(sum)
+
+        # Activaciones de salida
+        for k in range(len(self.urlids)):
+            sum = 0.0
+            for j in range(len(self.hiddenids)):
+                sum = sum + self.ah[j] * self.wo[j][k]
+            self.ao[k] = tanh(sum)
+
+        return self.ao[:]
+
+    def getresult(self, wordids, urlids):
+        self.setupnework(wordids, urlids)
+        return self.feedforward()
+
+    def backPropagate(self, targets, N=0.5):
+        # Calcular los errores para la salida
+        output_deltas = [0.0] * len(self.urlids)
+        for k in range(len(self.urlids)):
+            error = targets[k] - self.ao[k]
+            output_deltas[k] = dtanh(self.ao[k]) * error
+
+        # Calcular los errores de la capa oculta
+        hidden_deltas = [0.0] * len(self.hiddenids)
+        for j in range(len(self.hiddenids)):
+            error = 0.0
+            for k in range(len(self.urlids)):
+                error = error + output_deltas[k] * self.wo[j][k]
+            hidden_deltas[j] = dtanh(self.ah[j]) * error
+
+        # Actualizar los pesos de salida
+        for j in range(len(self.hiddenids)):
+            for k in range(len(self.urlids)):
+                change = output_deltas[k] * self.ah[j]
+                self.wo[j][k] = self.wo[j][k] + N * change
+
+        # Actualizar los pesos de entrada
+        for i in range(len(self.wordids)):
+            for j in range(len(self.hiddenids)):
+                change = hidden_deltas[j] * self.ai[i]
+                self.wi[i][j] = self.wi[i][j] + N * change
+
+    def trainquery(self, wordids, urlids, selectedurl):
+        # Generar un nodo oculto si es necesario
+        self. generatehiddennode(wordids, urlids)
+
+        self.setupnework(wordids, urlids)
+        self.feedforward()
+        targets = [0.0] * len(urlids)
+        targets[urlids.index(selectedurl)] = 1.0
+        self.backPropagate(targets)
+        self.updatedatabase()
+
+    def updatedatabase(self):
+        # Establecer los valores en la base de datos
+        for i in range(len(self.wordids)):
+            for j in range(len(self.hiddenids)):
+                self.setstrength(self.wordids[i], self.hiddenids[j],
+                        0, self.wi[i][j])
+
+        for j in range(len(self.hiddenids)):
+            for k in range(len(self.urlids)):
+                self.setstrength(self.hiddenids[j], self.urlids[k], 1,
+                        self.wo[j][k])
+
+        self.con.commit()
+
 def main():
     print "Ejemplos que aparecen en la red neuronal"
 
@@ -86,6 +199,41 @@ def main():
     mynet.generatehiddennode([wWorld, wBank], [uWorldBank, uRiver, uEarth])
     for c in mynet.con.execute('select * from wordhidden'): print c
     for c in mynet.con.execute('select * from hiddenurl'): print c
+    """
+
+    """
+    print "Ejemplo 2"
+    import nn
+    mynet = nn.searchnet('nn.db')
+    wWorld, wRiver, wBank = 101, 102, 103
+    uWorldBank, uRiver, uEarth = 201, 202, 203
+    mynet.getresult([wWorld, wBank], [uWorldBank, uRiver, uEarth])
+    """
+
+    """
+    print "Ejemplo 3"
+    import nn
+    mynet = nn.searchnet('nn.db')
+    wWorld, wRiver, wBank = 101, 102, 103
+    uWorldBank, uRiver, uEarth = 201, 202, 203
+    mynet.trainquery([wWorld, wBank], [uWorldBank, uRiver, uEarth], uWorldBank)
+    mynet.getresult([wWorld, wBank], [uWorldBank, uRiver, uEarth])
+    """
+
+    """
+    print "Ejemplo 4"
+    import nn
+    mynet = nn.searchnet('nn.db')
+    wWorld, wRiver, wBank = 101, 102, 103
+    uWorldBank, uRiver, uEarth = 201, 202, 203
+    allurls = [uWorldBank, uRiver, uEarth]
+    for i in range(30):
+    	mynet.trainquery([wWorld, wBank], allurls, uWorldBank)
+    	mynet.trainquery([wRiver, wBank], allurls, uRiver)
+    	mynet.trainquery([wWorld], allurls, uEarth)
+    mynet.getresult([wWorld, wBank], allurls)
+    mynet.getresult([wRiver, wBank], allurls)
+    mynet.getresult([wBank], allurls)
     """
 
 if __name__ == "__main__":
